a7705d2d94ea4e55cf3e2af603212fbcf0bdc67e
Trent Moss <trentmoss05@gmail.com>
2019-01-02 02:41:36 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,7 +2,7 @@
 
 def countdown(seconds_to_midnight)
   while seconds_to_midnight > 0
-    puts "#{x} SECOND(S)!"
+    puts "#{seconds_to_midnight} SECOND(S)!"
     seconds_to_midnight -= 1
     "HAPPY NEW YEAR!"
 end
